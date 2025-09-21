0574bfa2848bcd78835584b667062f6572506a91
Swastik Kulshreshtha <56737344+Swastik2442@users.noreply.github.com>
2025-09-21 18:30:22 +0530

    Automate API Setup

--- nms/README.md
+++ nms/README.md
@@ -2,13 +2,15 @@
 
 ## Setup
 
-1. Start Zabbix: `docker compose up -d`
-2. Start API Service: `uvicorn api:app --host=0.0.0.0 --port=5000`
-3. Go to [`localhost:8080`](http://localhost:8080) and login using username and password.
-4. Create a new Trigger Media Type of type Webhook and URL `http://host.docker.internal:5000/zabbix/webhook`.
-   1. Add the script in [`webhook-script.js`](./webhook-script.js) in the Script text box.
-   2. Add the Message Templates using the JSON strings in [`/templates`](./templates/).
-5. Create a new Media for the Zabbix Super-admin with the type as the created Media Type.
-6. Create a new Trigger Action with the operation as the created Media Type and User as Zabbix Super-admin.
+1. Start Zabbix, API Server, and other services: `docker compose up -d`
+2. Setup the API in Zabbix via
+   * Script: `python setupZabbix.py`
+   * Frontend:
+     1. Go to [`localhost:8080`](http://localhost:8080) and login using username and password.
+     2. Create a new Trigger Media Type of type Webhook and URL `http://host.docker.internal:5000/zabbix/webhook`.
+        1. Add the script in [`webhook-script.js`](./webhook-script.js) in the Script text box.
+        2. Add the Message Templates using the JSON strings in [`/templates`](./templates/).
+     3. Create a new Media for a user with the type as the created Media Type.
+     4. Create a new Trigger Action and a new Service Action with the operation as the created Media Type and User as selected earlier.
 
 > Make sure the Zabbix Server (zabbix-agent) host has its DNS set as `zabbix-agent`.
---
+++ nms/setupZabbix.py
@@ -0,0 +1,135 @@
+import os
+from collections.abc import Callable
+from typing import Literal
+
+from zabbix_utils import ZabbixAPI
+
+from templates import _json_files_content
+
+def _getWebhookScript() -> str:
+    with open(os.path.join(os.path.dirname(__file__), "webhook-script.js"), 'r', encoding='utf-8') as f:
+        whScript = f.read().strip()
+    return whScript
+
+def createMediaType(zapi: ZabbixAPI, apiUrl: str, mediaTypeId: str | None = None) -> str | None:
+    params = {
+        "type": "4",
+        "name": "Post to API",
+        "parameters": [
+            {"name": "Message", "value": "{ALERT.MESSAGE}"},
+            {"name": "Subject", "value": "{ALERT.SUBJECT}"},
+            {"name": "To", "value": "{ALERT.SENDTO}"},
+            {"name": "URL","value": apiUrl}
+        ],
+        "script": _getWebhookScript(),
+        "message_templates": [
+            {
+                "eventsource": "0",
+                "recovery": "0",
+                "subject": "Problem: {EVENT.NAME}",
+                "message": _json_files_content["problem_json"]
+            },
+            {
+                "eventsource": "0",
+                "recovery": "1",
+                "subject": "Resolved in {EVENT.DURATION}: {EVENT.NAME}",
+                "message": _json_files_content["problem_recovery_json"]
+            },
+            {
+                "eventsource": "0",
+                "recovery": "2",
+                "subject": "Updated problem in {EVENT.AGE}: {EVENT.NAME}",
+                "message": _json_files_content["problem_update_json"]
+            },
+            {
+                "eventsource": "4",
+                "recovery": "0",
+                "subject": "Service \"{SERVICE.NAME}\" problem: {EVENT.NAME}",
+                "message": _json_files_content["service_json"]
+            },
+            {
+                "eventsource": "4",
+                "recovery": "1",
+                "subject": "Service \"{SERVICE.NAME}\" resolved in {EVENT.DURATION}: {EVENT.NAME}",
+                "message": _json_files_content["service_recovery_json"]
+            },
+            {
+                "eventsource": "4",
+                "recovery": "2",
+                "subject": "Changed \"{SERVICE.NAME}\" service status to {EVENT.UPDATE.SEVERITY} in {EVENT.AGE}",
+                "message": _json_files_content["service_update_json"]
+            }
+        ]
+    }
+
+    method: Callable = zapi.mediatype.create # type: ignore
+    if mediaTypeId is not None:
+        method = zapi.mediatype.update # type: ignore
+        params["mediatypeid"] = mediaTypeId
+
+    response = method(params)
+
+    if mediaTypeId is not None:
+        assert hasattr(response, "mediatypeids") and mediaTypeId in response["mediatypeids"], "Updating Media Type failed"
+    else:
+        assert hasattr(response, "mediatypeids") and len(response["mediatypeids"]) > 0, "Creating Media Type failed"
+        return response["mediatypeids"][0]
+
+def createMedia(zapi: ZabbixAPI, apiUrl: str, userId: str, mediaTypeId: str) -> None:
+    response = zapi.user.update({ # type: ignore
+        "userid": str(userId),
+        "medias": [{
+            "mediatypeid": mediaTypeId,
+            "sendto": apiUrl
+        }]
+    })
+    assert hasattr(response, "userids") and str(userId) in response["userids"], "Could not add Media to User"
+
+def createAction(zapi: ZabbixAPI, userId: str, mediaTypeId: str, eventsource: Literal[0, 4], name: str = "Report to API") -> None:
+    response = zapi.action.create({ # type: ignore
+        "name": name,
+        "eventsource": eventsource,
+        "esc_period": "1h",
+        "operations": [{
+            "operationtype": 0,
+            "opmessage_usr": [{"userid": userId}],
+            "opmessage": {"mediatypeid": mediaTypeId}
+        }],
+        "recovery_operations": [{
+            "operationtype": 0,
+            "opmessage_usr": [{"userid": userId}],
+            "opmessage": {"mediatypeid": mediaTypeId}
+        }],
+        "update_operations": [{
+            "operationtype": 0,
+            "opmessage_usr": [{"userid": userId}],
+            "opmessage": {"mediatypeid": mediaTypeId}
+        }]
+    })
+    assert hasattr(response, "actionids") and len(response["actionids"]) > 0, "Could not create Action"
+
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    ZABBIX_URL = os.getenv("ZABBIX_URL")
+    ZABBIX_USER = os.getenv("ZABBIX_USER")
+    ZABBIX_PASSWORD = os.getenv("ZABBIX_PASSWORD")
+
+    zapi = ZabbixAPI(url=ZABBIX_URL, user=ZABBIX_USER, password=ZABBIX_PASSWORD)
+
+    userId = str(int(input("Enter the ID of the User for whom the Triggers have to be added: ")))
+    apiUrl = input("Enter the URL for the API: ").strip()
+
+    # Create Media Type
+    mediaTypeId = createMediaType(zapi, apiUrl)
+    assert mediaTypeId is not None
+
+    # Create Media for selected User
+    createMedia(zapi, apiUrl, userId, mediaTypeId)
+
+    # Create Actions with Media Type for selected User
+    createAction(zapi, userId, mediaTypeId, 0, "Report Triggers to API")
+    createAction(zapi, userId, mediaTypeId, 4, "Report Service Updates to API")
+
+    print("Triggers added successfully")
